432ccba744b7f99ce58bbab8e9ac1846351f8244
Raviv <raken100@gmail.com>
2025-01-25 09:21:55 +0200

    Implemented a test method for successful get

---
+++ tests/MyWebApi.Tests/ItemsControllerTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using MyWebApi.Controllers;
+using MyWebApi.Data;
+using MyWebApi.Models;
+using Moq;
+
+namespace MyWebApi.Tests;
+
+public class ItemsControllerTests
+{
+    [Fact]
+    public void GetItem_ReturnsOk_WhenItemExists()
+    {
+        // Arrange
+        var mockRepository = new Mock<IItemsRepository>();
+        var itemId = 1;
+        var expectedItem = new Item(itemId, "Test Item", 9.99m, DateTime.Now);
+        mockRepository.Setup(repo => repo.GetItem(itemId)).Returns(expectedItem);
+
+        var controller = new ItemsController(mockRepository.Object);
+
+        // Act
+        var result = controller.GetItem(itemId) as OkObjectResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+        Assert.Equal(expectedItem, result.Value);
+    }
+}
